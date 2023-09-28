15855231870ee4ff278237f41bd62b44ea003d51
billettc <charles@cbillette.com>
2023-09-28 10:42:54 -0400

    configurable valid time threshold

--- gnss-controller/message/handlers/timegetter.go
+++ gnss-controller/message/handlers/timegetter.go
@@ -9,17 +9,33 @@ import (
 )
 
 type TimeGetter struct {
-	timeSet chan time.Time
+	timeSet            chan time.Time
+	timeValidThreshold ubx.NavPvtValid
 }
 
-func NewTimeGetter(done chan time.Time) *TimeGetter {
-	return &TimeGetter{timeSet: done}
+func NewTimeGetter(timeValidThreshold string, done chan time.Time) *TimeGetter {
+
+	tg := &TimeGetter{timeSet: done}
+
+	switch timeValidThreshold {
+	case "date":
+		tg.timeValidThreshold = ubx.NavPvtValidDate
+	case "time":
+		tg.timeValidThreshold = ubx.NavPvtValidTime
+	case "resolved":
+		tg.timeValidThreshold = ubx.NavPvtFullyResolved
+	default:
+		panic(fmt.Errorf("invalid time valid threshold: %s", timeValidThreshold))
+	}
+
+	return tg
 }
 
 func (g *TimeGetter) HandleUbxMessage(message interface{}) error {
 	navPvt := message.(*ubx.NavPvt)
 	//fmt.Println("time getter nav pvt info, date validity:", navPvt.Valid, "accuracy:", navPvt.TAcc_ns, "lock type:", navPvt.FixType, "flags:", navPvt.Flags, "flags2:", navPvt.Flags2, "flags3:", navPvt.Flags3)
-	if navPvt.Valid&0x1 == 0 {
+
+	if navPvt.Valid&g.timeValidThreshold == 0 {
 		return nil
 	}
 	now := time.Date(int(navPvt.Year_y), time.Month(int(navPvt.Month_month)), int(navPvt.Day_d), int(navPvt.Hour_h), int(navPvt.Min_min), int(navPvt.Sec_s), int(navPvt.Nano_ns), time.UTC)
