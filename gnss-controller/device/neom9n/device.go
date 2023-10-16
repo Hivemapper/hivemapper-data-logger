950015beec8985825cdcc0b29061f964f19a0700
Aleksei Punov <design.qs@gmail.com>
2023-10-16 16:54:01 -0700

    Disable registration of MEASX messages if flag is not set

--- gnss-controller/device/neom9n/device.go
+++ gnss-controller/device/neom9n/device.go
@@ -103,9 +103,11 @@ func (n *Neom9n) Init(lastPosition *Position) error {
 	n.setConfig(546374490, []byte{0x01}, "CFG-MSGOUT-UBX_MON_RF_UART1")  // CFG-MSGOUT-UBX_MON_RF_UART1 0x2091035a Output rate of the UBX-MON-RF message on port UART1
 	n.setConfig(546373639, []byte{0x01}, "CFG-MSGOUT-UBX_NAV_PVT_UART1") // CFG-MSGOUT-UBX_NAV_PVT_UART1 0x20910007 Output rate of the UBX-NAV-PVT message on port UART1
 	n.setConfig(546373819, []byte{0x01}, "CFG-MSGOUT-NMEA_ID_GGA_UART1")
+	value := []byte{0x00}
 	if n.measxEnabled {
-		n.setConfig(546374149, []byte{0x01}, "CFG-MSGOUT-UBX_RXM_MEASX_UART1")
+		value = []byte{0x01}
 	}
+	n.setConfig(546374149, value, "CFG-MSGOUT-UBX_RXM_MEASX_UART1")
 	//todo: request UBX-SEC-ECSIGN message output
 	//todo: find CFG-MSGOUT-UBX_SEC_ECSIGN_UART1
 
@@ -195,7 +197,9 @@ func (n *Neom9n) Run(dataFeed *DataFeed, timeValidThreshold string, timeSetCallb
 	n.handlersRegistry.RegisterHandler(message.UbxMsgNavDop, dataFeed)
 	n.handlersRegistry.RegisterHandler(message.UbxMsgNavSat, dataFeed)
 	n.handlersRegistry.RegisterHandler(message.UbxMsgMonRf, dataFeed)
-	n.handlersRegistry.RegisterHandler(message.UbxRxmMeasx, dataFeed)
+	if n.measxEnabled {
+		n.handlersRegistry.RegisterHandler(message.UbxRxmMeasx, dataFeed)
+	}
 	n.handlersRegistry.RegisterHandler(message.NneaGGA, dataFeed)
 
 	if now == (time.Time{}) {
