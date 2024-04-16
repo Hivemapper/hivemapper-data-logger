2b445d5a0f61e0ebde9b880fc89e6a6dd33d40c1
Mazar Farran <mazar@hivemapper.com>
2024-04-15 17:29:58 -0700

    Only skip filtering

--- data/gnss/eventfeed.go
+++ data/gnss/eventfeed.go
@@ -94,34 +94,34 @@ func (f *GnssFeed) Run(gnssDevice *neom9n.Neom9n, timeValidThreshold string) err
 }
 
 func (f *GnssFeed) HandleData(d *neom9n.Data) {
-	if d.Dop.HDop > 5 {
-		fmt.Println("Rejecting data because HDOP is too high")
-		return
-	}
 
 	if !f.skipFiltering {
 
-		if f.lastGoodData == nil ||
-			math.Abs(f.lastGoodData.Latitude-d.Latitude) > 0.01 ||
-			math.Abs(f.lastGoodData.Longitude-d.Longitude) > 0.01 {
-			f.gnssFilteredData.init(d)
-		}
-		f.lastGoodData = d
+		if d.Dop.HDop > 6 {
+			if f.lastGoodData == nil ||
+				math.Abs(f.lastGoodData.Latitude-d.Latitude) > 0.01 ||
+				math.Abs(f.lastGoodData.Longitude-d.Longitude) > 0.01 {
+				f.gnssFilteredData.init(d)
+			}
+			f.lastGoodData = d
 
-		err := f.gnssFilteredData.lonFilter.Update(d.Timestamp, f.gnssFilteredData.lonModel.NewMeasurement(d.Longitude))
-		if err != nil {
-			panic("updating lon filter: " + err.Error())
-		}
-		err = f.gnssFilteredData.latFilter.Update(d.Timestamp, f.gnssFilteredData.latModel.NewMeasurement(d.Latitude))
-		if err != nil {
-			panic("updating lat filter: " + err.Error())
-		}
+			err := f.gnssFilteredData.lonFilter.Update(d.Timestamp, f.gnssFilteredData.lonModel.NewMeasurement(d.Longitude))
+			if err != nil {
+				panic("updating lon filter: " + err.Error())
+			}
+			err = f.gnssFilteredData.latFilter.Update(d.Timestamp, f.gnssFilteredData.latModel.NewMeasurement(d.Latitude))
+			if err != nil {
+				panic("updating lat filter: " + err.Error())
+			}
 
-		filteredLon := f.gnssFilteredData.lonModel.Value(f.gnssFilteredData.lonFilter.State())
-		filteredLat := f.gnssFilteredData.latModel.Value(f.gnssFilteredData.latFilter.State())
+			filteredLon := f.gnssFilteredData.lonModel.Value(f.gnssFilteredData.lonFilter.State())
+			filteredLat := f.gnssFilteredData.latModel.Value(f.gnssFilteredData.latFilter.State())
 
-		d.Longitude = filteredLon
-		d.Latitude = filteredLat
+			d.Longitude = filteredLon
+			d.Latitude = filteredLat
+		} else {
+			fmt.Printf("Skipping adding gnss entry to filter due to high HDOP: %v", d.Dop.HDop)
+		}
 	}
 
 	for _, handler := range f.dataHandlers {
