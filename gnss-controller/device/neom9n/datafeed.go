8d4674aa4b8d4a6f6e18f4b339f8c89791395ced
derekhive <derek@hivemapper.com>
2025-04-21 13:08:39 -0700

    change to 4Hz rate

--- gnss-controller/device/neom9n/datafeed.go
+++ gnss-controller/device/neom9n/datafeed.go
@@ -286,7 +286,7 @@ func (df *DataFeed) HandleUbxMessage(msg interface{}) error {
 		data.HorizontalAccuracy = float64(m.HAcc_mm) / 1000
 		data.VerticalAccuracy = float64(m.VAcc_mm) / 1000
 
-		if prevItowMs != 0 && m.ITOW_ms-prevItowMs > 125 {
+		if prevItowMs != 0 && m.ITOW_ms-prevItowMs > 250 {
 			fmt.Println("[WARNING] NavPvt drop of", m.ITOW_ms-prevItowMs, "ms (", prevItowMs, ",", m.ITOW_ms, ")")
 		}
 		prevItowMs = m.ITOW_ms
